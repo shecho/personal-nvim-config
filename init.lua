d12d90c7f0bd02b64fbf7a2877085c5386ce6ae8
shecho <shechovala@gmail.com>
2023-09-18 19:28:38 -0500

    some changes.

--- init.lua
+++ init.lua
@@ -1,14 +1,12 @@
 if vim.loader then
-    vim.loader.enable()
-    vim.schedule(function() vim.notify("nvim loader is enabled") end)
+  vim.loader.enable()
+  vim.schedule(function()
+    vim.notify("nvim loader is enabled")
+  end)
 end
 
 vim.cmd([[
 source $HOME/.config/nvim/general/settings.vim
-"source $HOME/.config/nvim/general/functions.vim
-]])
-vim.cmd([[
-  "source $HOME/.config/nvim/keys/which-key.vim
 ]])
 
 require("user.keymaps")
--- lua/shecho/core/functions.lua
+++ lua/shecho/core/functions.lua
@@ -59,9 +59,7 @@ function M.smart_quit()
   local bufnr = vim.api.nvim_get_current_buf()
   local modified = vim.api.nvim_buf_get_option(bufnr, "modified")
   if modified then
-    vim.ui.input({
-      prompt = "You have unsaved changes. Quit anyway? (y/n) ",
-    }, function(input)
+    vim.ui.input({ prompt = "You have unsaved changes. Quit anyway? (y/n) " }, function(input)
       if input == "y" then
         vim.cmd("q!")
       end
