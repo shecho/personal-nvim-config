a407c21751e5aef53f58a235226b21a32f154bc5
Sergio <shechovala@gmail.com>
2024-05-28 17:46:47 -0500

    Main (#16)
    * Mac lazy (#6)
    
    * some updates
    
    * some changes to include copilot
    
    * some las changes
    
    * some chhanges and added harpoon
    
    * some coc changes
    
    * some coco changes, lua files  and harpoon changes
    
    * more lsp config and some new lua stuff
    
    * refactor
    
    * removed client_id
    
    * git blame and some remaps
    
    * Adding more changes
    
    * some more changes
    
    * fox explorer issues
    
    * migrated telescope lo lua
    
    * Some refactors
    
    * adding fzs
    
    * adding some more changes a new plugins
    
    * some changes refactor
    
    * remove some stuff
    
    * Some refactor changes
    
    * more changes
    
    * disble lsp
    
    * fix theme errors
    
    * Add cmp fixes
    
    * more configs
    
    * some more changes.
    
    * Adding future keys lua
    
    * Add some changes to cmp
    
    * some refactors
    
    * some changes on copilot
    
    * some config
    
    * some saga config
    
    * some more changes.
    
    * remove some unused keymaps
    
    * some more changes
    
    * some changes..
    
    * change ordewr
    
    * change import order
    
    * better changes an d workflow
    
    * Mac (#2)
    
    go
    
    * some cmp adjust
    
    * some ui changes
    
    * illuminate added
    
    * aliminate integrations a customization
    
    * some changes on color handling.
    
    * remove mini map
    
    * some changes after update.
    
    * some more changes after update.
    
    * some format
    
    * some reduces changes.
    
    * some changes.
    
    * minimal changes to be faster
    
    * some mayor changes.
    
    * some lua changes.
    
    * looks like working,.
    
    * some chag,es
    
    * some ui changes.
    
    * lua folder structure
    
    * fix some icons and remove some plugins config.
    
    * some ui changes.
    
    * some changes on mac
    
    * some changes and fix explorer
    
    * some basic changes.
    
    * some start screen changes.
    
    * some set up changes.
    
    * some changes. after vim lazy
    
    * add some skel
    
    * more changes
    
    * some super mega changes.
    
    * more extensions.
    
    * add  mayour changes and missing pluggins
    
    * some lsp config changes
    
    * cmp changes.
    
    * Mac lua (#4)
    
    * some ui changes.
    
    * some changes on mac
    
    * some changes and fix explorer
    
    * some basic changes.
    
    * some start screen changes.
    
    * some changes.
    
    * remove  unused files
    
    * some super changes.
    
    * remove dupped pluggins
    
    * more changes.
    
    * more love to comments.
    
    * changed all
    
    * remove code.
    
    * some renames.
    
    * some extra changes and  keymaps
    
    * remaps
    
    * remove unsuded code.
    
    * remaps.
    
    * some changes.
    
    * remove mapping move migrate missing lua keymaps and some refactor.
    
    * some minimal changes
    
    * remove some pluggis dupped config
    
    * some minimal ui changes.
    
    * some minimal ui fixes
    
    * more maps renames and more changes.
    
    * some more changes
    
    * some rename and move files to user folder, dele te users folder.
    
    * some buffer keymaps
    
    * json update
    
    * just dep updates
    
    * some remaps
    
    * colorscheme changes.
    
    * fix formatting function from lsp
    
    * keymaps and colorscheme changes
    
    * Mac lazy (#14)
    
    * remove some dupped pluggins.
    
    * some minimal changes
    
    * remove some pluggis dupped config
    
    * some minimal ui changes.
    
    * some minimal ui fixes
    
    * more maps renames and more changes.
    
    * some more changes
    
    * some rename and move files to user folder, dele te users folder.
    
    * some buffer keymaps
    
    * json update
    
    * just dep updates
    
    * some remaps
    
    * colorscheme changes.
    
    * fix formatting function from lsp
    
    * keymaps and colorscheme changes

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -20,74 +20,82 @@ return {
 
         syntax on
         colorscheme one
+        " call one#highlight('@none', '', '', 'bold')
+        " call one#highlight('@text', '', '000000', 'bold')
+        " call one#highlight('@variable.parameter', 'e5c07b', '000000', 'bold')
+        " call one#highlight('@variable.member', '', '000000', 'bold')
 
-        " call one#highlight('@Constant', '', '', 'bolditalic')
-        " call one#highlight('@variable', '', '', 'bold')
-        " call one#highlight('@variable', '528bff', '', 'bold')"
-        " call one#highlight('@attribute', '', '', 'bold,standout')
-        " call one#highlight('Tag', 'B243E6', '', 'bold')
-        " call one#highlight('@Tag', '528bff', '', 'bold')
-        call one#highlight('normal', '', '', 'bold')
-        call one#highlight('Special', '', '', 'bold')
-        call one#highlight('Constant', '', '', 'bold')
-        call one#highlight('@constant.builtin', 'c678dd', '', 'bold')
-        call one#highlight('@Keyword', 'c678dd', '', 'italic')"reserved words
-        call one#highlight('@keyword.function', 'c678dd', '', 'bolditalic')
-        call one#highlight('@keyword.operator', '', '', 'italic')
-        call one#highlight('@Parameter', 'abb2bf', '', 'bolditalic,')"params abd arguments and variables names
-        call one#highlight('@Parameter.reference', 'abb2bf', '', 'bolditalic,')"params abd arguments and variables names
-        call one#highlight('@method', '528bff', '', 'italic')
-        call one#highlight('@property', 'd19a66', '', 'bolditalic')
-        call one#highlight('Boolean', '', '', 'bolditalic')
-        call one#highlight('Conditional', '', '', 'italic')
-        call one#highlight('SpecialChar', '', '', 'bold')
-        call one#highlight('Character', '', '', 'bold')
-        call one#highlight('Include', 'c678dd', '', 'italic')"imports/exports
-        call one#highlight('@Tag', '', '', 'bold')
-        call one#highlight('Number', '', '', 'bold')
-        call one#highlight('Float', '', '', 'bold')
+        call one#highlight('@variable', 'abb2bf', '', '')
+        call one#highlight('normalfloat', '', '282C34', 'bold')
         call one#highlight('String', '', '', 'bold')
         call one#highlight('Repeat', '', '','italic')"
         call one#highlight('Type', '', '', 'bold')
+        call one#highlight('@Keyword', 'c678dd', '', 'italic')"reserved words
+        call one#highlight('@Constant', '', '', 'bolditalic')
+        call one#highlight('@variable.builtin', '', '', 'bold')
+        call one#highlight('@variable.member', 'e06c75', '', 'bold')
+        call one#highlight('@variable.parameter', 'e06c75', '', 'bold')
+        call one#highlight('@variable', '', '', 'bold')"
+        call one#highlight('normal', '', '', 'bold')
+        call one#highlight('Delimiter','abb2bf', '', 'bold')"brackets () {} []
+        call one#highlight('MatchParen', '', '', 'underline,bolditalic')
+        " call one#highlight('@attribute', '', '', 'bold')
+        " call one#highlight('Tag', 'B243E6', '000000', 'bold')
+        call one#highlight('@Tag', 'e5c07b', '', 'bold')
+        call one#highlight('@Tag.delimiter', 'abb2bf', '', 'bold') " #B243E6 </>
+        call one#highlight('@Tag.attribute', 'd19a66', '', 'bolditalic') " #B243E6
+        " call one#highlight('Special', '', '', 'bold')
+        call one#highlight('Constant', '', '', 'bold')
+        call one#highlight('@constant.builtin', 'c678dd', '', 'bold')
+        call one#highlight('@keyword.function', '528bff', '', 'bolditalic')
+        " call one#highlight('@keyword.operator', '', '000000', 'italic')
+        " call one#highlight('@Parameter', '', '000000', 'bolditalic,')"params abd arguments and variables names
+        " call one#highlight('@Parameter.reference', '', '528bff', 'bolditalic,')"params abd arguments and variables names
+        call one#highlight('@method', '528bff', '', 'italic')
+        call one#highlight('@property', 'e06c75', '', 'bolditalic') " #
+        call one#highlight('Boolean', '', '', 'bolditalic')
+        call one#highlight('Conditional', 'B243E6', '', 'italic')
+        " call one#highlight('SpecialChar', '', '', 'bold,standout')
+        " call one#highlight('Character', '', '', 'bold')
+        " call one#highlight('@character', '', '', 'bold')
+        " call one#highlight('Include', 'c678dd', '000000', 'italic')
+        " call one#highlight('Number', '', '', 'bold')
+        " call one#highlight('Float', '', '', 'bold')
         call one#highlight('@type.definition', '', '', 'bolditalic')
         call one#highlight('Asynchronous', '', '', 'italic')
         call one#highlight('Exception', '', '', 'italic') " try catch
-        call one#highlight('StorageClass', '', '','bolditalic')" #B243E6
-        call one#highlight('Error', 'be5046', '', 'bolditalic')"#e06c75 #be5046 #e45649 #ca1243  #c18401 #e5c07b #d19a66 #61afef #528bff 528bff #4078f2 4078f2 #abb2bf
+        call one#highlight('StorageClass', '', '','bolditalic')" #B243E6 #8a3fa0  #7e3992  #c678dd #282C34 #abb2bf #528bff #181a1f #282c34 #31353f #393f4a  #3b3f4c  #0e1013  #151820 
+        call one#highlight('Error', 'be5046', '', 'bolditalic')" #e06c75 #be5046 #e45649 #ca1243 #c18401 #e5c07b #d19a66 #56B6C2 #61afef #528bff #4078f2
         call one#highlight('ErrorMsg', 'e06c75', '', 'bold')
         call one#highlight('DiagnosticVirtualTextError', 'e06c75', '', 'bolditalic')
         call one#highlight('DiagnosticError', 'e06c75', '', 'bolditalic')
         call one#highlight('DiagnosticFloatingError', 'e06c75', '', 'bolditalic')
         call one#highlight('DiagnosticSignError', 'e06c75', '', 'bolditalic')
         call one#highlight('DiagnosticUnderlineError', 'e06c75', '', 'bolditalic,underline')
-        " call one#highlight('@Tag.delimiter', '', '', '')
-        call one#highlight('@Tag.delimiter', 'abb2bf', '', 'bold') " #B243E6 </>
-        call one#highlight('Delimiter','', '', 'bold')"brackets () {} []
-        call one#highlight('MatchParen', '', '', 'underline,bolditalic')
-        call one#highlight('@operator', 'c678dd', '', 'bold')" *= - + =>'#c678dd c678dd
-        call one#highlight('Operator', '', '', '')" *= - + =>'#c678dd#
-        " call one#highlight('@function', '61afef', '', 'bold')"#61afef
-        " call one#highlight('Function', '61afef', '','bold')"
+        call one#highlight('@operator', 'c678dd', '', '')" *= - + =>'
+        " call one#highlight('@punctuation.bracket', '', '', 'bold')" *= - + =>
+        " call one#highlight('@Punctuation.delimiter', '', '', 'bold')
+        " call one#highlight('Operator', '', '000000', '')" *= - + =>'
+        " call one#highlight('@punctuation.delimiter', 'c678dd', '', 'bold')" *= - + =>'
+        " call one#highlight('@function', '61afef', '', 'bold')"
+        " call one#highlight('@function.method', '528bff', '', 'bold')"
+        " call one#highlight('@function.builtin', '528bff', '', 'bolditalic')"
+        " call one#highlight('@function.macro', '528bff', '', 'bolditalic')"
+        call one#highlight('@text.uri', '', '', 'bolditalic,underline')"
         " call one#highlight('Identifier', '', '', 'bold')"
         " call one#highlight('Keyword', 'c678dd', '', 'italic')"reserved words
-        " call one#highlight('@Punctuation', '', '', 'bold')
-        " call one#highlight('@Punctuation.delimiter', '', '', 'bold')
         " call one#highlight('@Punctuation.bracket', '', '', 'bold')
-        " call one#highlight('@Punctuation', '', '', 'bold')
         call one#highlight('Directory', '', '', 'bold')
-        " call one#highlight('Label', '', '', 'bold,underline')"html and jsx
+        " call one#highlight('@label', '61afef', '', 'bold')"html and jsx
         " call one#highlight('WildMenu', '', '', 'reverse')
         " call one#highlight('Use', '', '', 'standout')
         " call one#highlight('Utility', '', '', 'standout')
         " call one#highlight('Select', '', '', 'standout')
         call one#highlight('Visual', '', '', 'bolditalic')
-        " call one#highlight('TTabLineSelabLineSel', '', '3e4452', 'bolditalic,underline')
-        " call one#highlight('TabLineSel', '', '3e4452', 'bolditalic')
-        " call one#highlight('TabLineFill', '', '', '')
         call one#highlight('TabLine', '', '', 'bold')
         " call one#highlight('Search', '', '', 'bolditalic')
         " call one#highlight('IncSearch', '', '', 'bolditalic,standout')
-        call one#highlight('Statement', '', '', 'bold')
+        " call one#highlight('Statement', '', '', 'bold')
         call one#highlight('CursorColumn', '828997', '', 'standout')" word #4b5263 #5c6370  5c6370 #828997 #abb2bf  abb2bf
         " call one#highlight('CursorLine', '', '', 'standout')
         " call one#highlight('Cursor', '', '', 'standout')
@@ -104,6 +112,10 @@ return {
         " italic
         " standout
         " --------------------------------------------------------------
+        call one#highlight('normal', 'abb2bf', '282C34', 'bold')
+        call one#highlight('@variable.builtin', 'd19a66', '', 'italic')
+
+        " call one#highlight('normal', 'abb2bf', '282C34', 'bold')
 
         let g:VM_theme = 'purplegray'
         hi illuminatedWord gui=bolditalic guifg=#828997
@@ -111,7 +123,7 @@ return {
         hi IlluminatedWordRead gui=bolditalic guifg=#828997
         if (has("termguicolors"))
             set termguicolors
-            hi LineNr ctermbg=NONE guibg=NONE
+            " hi LineNr ctermbg=NONE guibg=NONE
         endif
       ]])
     end,
--- lua/user/plugins/lsp/lspconfig.lua
+++ lua/user/plugins/lsp/lspconfig.lua
@@ -12,7 +12,7 @@ return {
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local keymap = vim.keymap
 
-    local on_attach = function(client, bufnr)
+    local on_attach = function(_, bufnr)
       local opts = { noremap = true, silent = true, buffer = bufnr }
       opts.buffer = bufnr
       -- set keybinds
@@ -39,8 +39,8 @@ return {
       keymap.set("n", "<leader>lT", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
       opts.desc = "See available code actions"
-      keymap.set({ "n", "v" }, "<leader>la", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
-      keymap.set({ "n", "v" }, "<leader>lq", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+      keymap.set({ "n", "v" }, "<leader>lq", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+      keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
       opts.desc = "rename"
       keymap.set("n", "<leader>le", vim.lsp.buf.rename, opts) -- smart rename
@@ -49,16 +49,17 @@ return {
 
       opts.desc = "Show buffer diagnostics"
       keymap.set("n", "<leader>lD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+      keymap.set("n", "<leader>ll", "<cmd>Lspsaga show_buf_diagnostics", opts) -- show  diagnostics for file
 
       opts.desc = "Go to previous diagnostic"
       keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
       opts.desc = "Go to next diagnostic"
-      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+      keymap.set("n", "<leader>ln", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+      keymap.set("n", "<leader>lN", "<cmd>Lspsaga diagnostic_jump_next", opts) -- jump to next diagnostic in buffer
 
-      opts.desc = "Show documentation for what is under cursor"
+      opts.desc = "Show documentation under cursor"
       keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
       opts.desc = "Show saga hover"
       keymap.set("n", "<leader>k", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
       opts.desc = "Toggle winbar"
@@ -66,7 +67,6 @@ return {
 
       opts.desc = "Format"
       keymap.set("n", "<leader>lf", vim.lsp.buf.format, opts) -- nnoremap <leader>lf :lua vim.lsp.buf.format({async = true})<CR>
-      keymap.set("n", "<leader>lF", vim.lsp.buf.formating, opts) --  nnoremap <leader>lF :lua vim.lsp.buf.formating()<CR>
 
       -- opts.desc = "Restart LSP"
       -- keymap.set("n", "<leader>lr", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
@@ -89,46 +89,35 @@ return {
     -- configure html server
     lspconfig["html"].setup({
       capabilities = capabilities,
-      on_attach = function(_, bufnr)
-        on_attach(_, bufnr)
-      end,
+      on_attach = on_attach,
     })
 
     -- configure css server
     lspconfig["cssls"].setup({
       capabilities = capabilities,
-      on_attach = function(_, bufnr)
-        on_attach(_, bufnr)
-      end,
+      on_attach = on_attach,
     })
 
     -- configure tailwindcss server
     lspconfig["tailwindcss"].setup({
       capabilities = capabilities,
-      on_attach = function(_, bufnr)
-        on_attach(_, bufnr)
-      end,
+      on_attach = on_attach,
     })
 
     lspconfig["svelte"].setup({
       capabilities = capabilities,
-      on_attach = function(_, bufnr)
-        on_attach(_, bufnr)
-      end,
+      on_attach = on_attach,
     })
 
     lspconfig["prismals"].setup({
       capabilities = capabilities,
-      on_attach = function(_, bufnr)
-        on_attach(_, bufnr)
-      end,
+      on_attach = on_attach,
     })
 
     lspconfig["graphql"].setup({
       capabilities = capabilities,
-      on_attach = function(_, bufnr)
-        on_attach(_, bufnr)
-      end,
+      on_attach = on_attach,
+
       filetypes = {
         "graphql",
         "gql",
@@ -141,9 +130,9 @@ return {
     -- configure emmet language server
     lspconfig["emmet_ls"].setup({
       capabilities = capabilities,
-      on_attach = function(_, bufnr)
-        on_attach(_, bufnr)
-      end,
+
+      on_attach = on_attach,
+
       filetypes = {
         "html",
         "typescriptreact",
@@ -159,9 +148,7 @@ return {
     -- configure python server
     lspconfig["pyright"].setup({
       capabilities = capabilities,
-      on_attach = function(_, bufnr)
-        on_attach(_, bufnr)
-      end,
+      on_attach = on_attach,
     })
     lspconfig.clangd.setup({
       capabilities = capabilities,
@@ -169,24 +156,18 @@ return {
         "clangd",
         "--offset-encoding=utf-16",
       },
-      on_attach = function(_, bufnr)
-        on_attach(_, bufnr)
-      end,
+      on_attach = on_attach,
     })
 
     lspconfig.flow.setup({
-      on_attach = function(_, bufnr)
-        on_attach(_, bufnr)
-      end,
+      on_attach = on_attach,
       capabilities = capabilities,
     })
 
     typescript.setup({
       server = {
         capabilities = capabilities,
-        on_attach = function(_, bufnr)
-          on_attach(_, bufnr)
-        end,
+        on_attach = on_attach,
       },
       filetypes = {
         "javascript",
@@ -197,28 +178,25 @@ return {
         "typescript.tsx",
       },
     })
-    -- lspconfig["tsserver"].setup({
-    --   capabilities = capabilities,
-    --   cmd = { "typescript-language-server", "--stdio" },
-    --   on_attach = function(_, bufnr)
-    --     on_attach(_, bufnr)
-    --   end,
-    --   filetypes = {
-    --     "javascript",
-    --     "javascriptreact",
-    --     "javascript.jsx",
-    --     "typescript",
-    --     "typescriptreact",
-    --     "typescript.tsx",
-    --   },
-    -- })
+    lspconfig["tsserver"].setup({
+      capabilities = capabilities,
+      cmd = { "typescript-language-server", "--stdio" },
+      on_attach = on_attach,
+      h,
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "javascript.jsx",
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+      },
+    })
 
     -- configure lua server (with special settings)
     lspconfig["lua_ls"].setup({
       capabilities = capabilities,
-      on_attach = function(_, bufnr)
-        on_attach(_, bufnr)
-      end,
+      on_attach = on_attach,
       settings = { -- custom settings for lua
         Lua = {
           -- make the language server recognize "vim" global
